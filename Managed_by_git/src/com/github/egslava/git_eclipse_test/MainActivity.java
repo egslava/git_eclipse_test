ee29159d801b0f25111aa23b27484fd572f42c55
Slava Egorenkov <ve@poloniumarts.com>
2013-04-03 15:03:35 +0400

    Fixed some errors

--- Managed_by_git/src/com/github/egslava/git_eclipse_test/MainActivity.java
+++ Managed_by_git/src/com/github/egslava/git_eclipse_test/MainActivity.java
@@ -1,8 +1,12 @@
 package com.github.egslava.git_eclipse_test;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity 
                         implements OnClickListener{
