39b0401167f4d25aa899a1e3e5a7e311255a8d94
Slava Egorenkov <ve@poloniumarts.com>
2013-04-03 14:37:42 +0400

    Added button and message listener

--- Managed_by_git/src/com/github/egslava/git_eclipse_test/MainActivity.java
+++ Managed_by_git/src/com/github/egslava/git_eclipse_test/MainActivity.java
@@ -2,14 +2,26 @@ package com.github.egslava.git_eclipse_test;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.pm.LabeledIntent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
-    @Override
+    private Button changeTextButton;
+	private TextView helloTextView;
+
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helloTextView = (TextView) findViewById(R.id.textView1);
+        changeTextButton = (Button) findViewById(R.id.changeTextButton);
+        changeTextButton.setOnClickListener(this);
     }
 
 
@@ -19,5 +31,16 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onClick(View arg0) {
+		switch(arg0.getId()){
+		case R.id.changeTextButton:
+			helloTextView.setText("You've clicked on button!");
+			break;
+		}
+		
+	}
     
 }
